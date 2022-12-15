a48710997c0140cbe768b29f9c0bc3ad15f912da
Sina Zakizadeh <120073344+SinaZakizadeh@users.noreply.github.com>
2022-12-15 22:11:05 +0330

    Add files via upload

---
+++ Prj 3/Code/script.js
@@ -0,0 +1,22 @@
+// In the Name of Allah
+
+// Project Title : Prj 3
+// Programming Software : Visual Studio Code , Version 1.73.1
+// Programming language : JavaScript
+// Programmer : Sina Zakizadeh
+
+// Project Description :
+// 1. Set the value of 'language' to the language spoken where you live (some countries have multiple languages, but just choose one)
+// 2. Think about which variables should be const variables (which values will never change, and which might change?). Then, change these variables to const.
+// 3. Try to change one of the changed variables now, and observe what happens
+
+// Project Solution :
+
+// const => Country , Continent , language
+// Let => Population
+let language;
+language = 'Persian';
+let population = 85; // for 2021 in  millions
+console.log(population);
+population = 90; // for example for 2025 in millions
+console.log(population);
