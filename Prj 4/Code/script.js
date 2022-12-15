dcb164aabefb782627b7b60ddf52b296eb728273
Sina Zakizadeh <120073344+SinaZakizadeh@users.noreply.github.com>
2022-12-15 22:11:24 +0330

    Add files via upload

---
+++ Prj 4/Code/script.js
@@ -0,0 +1,37 @@
+// In the Name of Allah
+
+// Project Title : Prj 4
+// Programming Software : Visual Studio Code , Version 1.73.1
+// Programming language : JavaScript
+// Programmer : Sina Zakizadeh
+
+// Project Description :
+// 1. If your country split in half, and each half would contain half the population, then how many people would live in each half?
+// 2. Increase the population of your country by 1 million and log the result to the console
+// 3. Finland has a population of 6 million. Does your country have more people than Finland?
+// 4. The average population of a country is 33 million people. Does your country have less people than the average country?
+// 5. Based on the variables you created, create a new variable 'description' which contains a string with this format: 'Portugal is in Europe, and its 11 million people speak portuguese
+
+// Project Solution :
+
+// 1.
+let population = 85;
+let newPopulation = population / 2;
+console.log(newPopulation);
+
+// 2.
+population++;
+console.log(population);
+
+// 3.
+let myCountrysPopulation = 85;
+let finlandsPopulation = 6;
+console.log(myCountrysPopulation > finlandsPopulation);
+
+// 4.
+let averagePopulation = 33;
+console.log(myCountrysPopulation < averagePopulation);
+
+// 5.
+const description = `Portugal is in Europe, and its 11 million people speak portuguese`;
+console.log(description);
