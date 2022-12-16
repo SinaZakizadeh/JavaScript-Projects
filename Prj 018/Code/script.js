7c8a627410752bee691824e3a96fb49105409c7a
Sina Zakizadeh <120073344+SinaZakizadeh@users.noreply.github.com>
2022-12-16 20:41:32 +0330

    Add files via upload

---
+++ Prj 018/Code/script.js
@@ -0,0 +1,20 @@
+// In the Name of Allah
+
+// Project Title : Prj 18
+// Programming Software : Visual Studio Code , Version 1.73.1
+// Programming language : JavaScript
+// Programmer : Sina Zakizadeh
+// JavaScript Projects :https://github.com/SinaZakizadeh/JavaScript-Projects.git
+
+// Project Description :
+// Create an object called 'myCountry' for a country of your choice, containing properties 'country', 'capital', 'language', 'population' and 'neighbours'
+
+// Project Solution :
+
+const myCountry = {
+  country: 'Iran',
+  captital: 'Tehran',
+  language: 'Persian',
+  population: 85,
+  neighbours: ['Iraq', 'Turkey', 'Afghanistan', 'Pakistan'],
+};
