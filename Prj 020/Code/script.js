14240432c5970bf58e38fdf8d09a13a3edb89025
Sina Zakizadeh <120073344+SinaZakizadeh@users.noreply.github.com>
2022-12-20 02:41:13 +0330

    Add files via upload

---
+++ Prj 020/Code/script.js
@@ -0,0 +1,44 @@
+// In the Name of Allah
+
+// Project Title : Prj 20
+// Programming Software : Visual Studio Code , Version 1.73.1
+// Programming language : JavaScript
+// Programmer : Sina Zakizadeh
+// JavaScript Projects :https://github.com/SinaZakizadeh/JavaScript-Projects.git
+
+// Project Description :
+// Mark and John are trying to compare their BMI (Body Mass Index), which is
+// calculated using the formula:
+// BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+// and height in meter).
+// 1. Store Mark's and John's mass and height in variables
+// 2. Calculate both their BMIs using the formula
+// 3. Create a Boolean variable 'markHigherBMI' containing information about
+// whether Mark has a higher BMI than John.
+// § Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+// § Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+// Project Solution :
+
+const mark = {
+  Weight1: 78,
+  Weight2: 95,
+  Height1: 1.69,
+  Height2: 1.88,
+};
+const john = {
+  Weight1: 92,
+  Weight2: 85,
+  Height1: 1.95,
+  Height2: 1.76,
+};
+const BMI = (mass, height) => mass / height ** 2;
+const marksHigherBMI1 =
+  BMI(mark.Weight1, mark.Height1) > BMI(john.Weight1, john.Height1);
+const marksHigherBMI2 =
+  BMI(mark.Weight2, mark.Height2) > BMI(john.Weight2, john.Height2);
+if (marksHigherBMI1) {
+  console.log(`Based on Data 1 , Mark's BMI is Higher than John's`);
+} else console.log(`Based on Data 1 , Johns's BMI is Higher than Mark's`);
+if (marksHigherBMI2) {
+  console.log(`Based on Data 2 , Mark's BMI is Higher than John's`);
+} else console.log(`Based on Data 2 , Johns's BMI is Higher than Mark's`);
