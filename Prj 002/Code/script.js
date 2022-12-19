c268b548831a3c0e8f738c43d25b027502b9ad50
Sina Zakizadeh <120073344+SinaZakizadeh@users.noreply.github.com>
2022-12-19 20:00:05 +0330

    Add files via upload

---
+++ Prj 002/Code/script.js
@@ -0,0 +1,21 @@
+// In the Name of Allah
+
+// Project Title : Prj 2
+// Programming Software : Visual Studio Code , Version 1.73.1
+// Programming language : JavaScript
+// Programmer : Sina Zakizadeh
+
+// Project Description :
+// 1. Declare a variable called 'isIsland' and set its value according to yourcountry. The variable should hold a Boolean value. Also declare a variable'language', but don't assign it any value yet
+// 2. Log the types of 'isIsland', 'population', 'country' and 'language'to the console
+
+// Project Solution :
+
+const country = 'Iran';
+let population = 85; //for 2021 in million
+const isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
