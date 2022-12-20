7d9fc99ab846b81baaecee72188503384950f45c
Sina Zakizadeh <120073344+SinaZakizadeh@users.noreply.github.com>
2022-12-21 00:19:16 +0330

    Add files via upload

---
+++ Prj 022/Code/script.js
@@ -0,0 +1,45 @@
+// In the Name of Allah
+
+// Project Title : Prj 22
+// Programming Software : Visual Studio Code , Version 1.73.1
+// Programming language : JavaScript
+// Programmer : Sina Zakizadeh
+// JavaScript Projects :https://github.com/SinaZakizadeh/JavaScript-Projects.git
+
+// Project Description :
+// Steven wants to build a very simple tip calculator for whenever he goes eating in a
+// restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and
+// 300. If the value is different, the tip is 20%.
+// Your tasks:
+// 1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for
+// this. It's not allowed to use an if/else statement 😅 (If it's easier for you, you can
+// start with an if/else statement, and then try to convert it to a ternary
+// operator!)
+// 2. Print a string to the console containing the bill value, the tip, and the final value
+// (bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value
+// 316.25”
+// Test data:
+// § Data 1: Test for bill values 275, 40 and 430
+
+// Project Solution :
+let bill = 275;
+let tip = 50 <= bill && bill <= 300 ? 0.15 * bill : 0.2 * bill;
+console.log(
+  `The Bill was ${bill} , the tip was ${tip} , and the total value was ${
+    bill + tip
+  }`
+);
+bill = 40;
+tip = 50 <= bill <= 300 ? 0.15 * bill : 0.2 * bill;
+console.log(
+  `The Bill was ${bill} , the tip was ${tip} , and the total value was ${
+    bill + tip
+  }`
+);
+bill = 430;
+tip = 50 <= bill <= 300 ? 0.15 * bill : 0.2 * bill;
+console.log(
+  `The Bill was ${bill} , the tip was ${tip} , and the total value was ${
+    bill + tip
+  }`
+);
