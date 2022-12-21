358aa6162ee4c9b97083415ecdf2ecb4deadf5d5
Sina Zakizadeh <120073344+SinaZakizadeh@users.noreply.github.com>
2022-12-21 17:29:40 +0330

    Add files via upload

---
+++ Prj 037/Code/script.js
@@ -0,0 +1,18 @@
+// In the Name of Allah
+
+// Project Title : Prj 37
+// Programming Software : Visual Studio Code , Version 1.74.2
+// Programming language : JavaScript
+// Programmer : Sina Zakizadeh
+// JavaScript Projects :https://github.com/SinaZakizadeh/JavaScript-Projects.git
+
+// Project Description :
+
+// 1. There are elections in your country! In a small town, there are only 50 voters.
+// Use a for loop to simulate the 50 people voting, by logging a string like this to
+// the console (for numbers 1 to 50): 'Voter number 1 is currently voting
+
+// Project Solution :
+for (let i = 1; i <= 50; i++) {
+  console.log(`Voter number ${i} is currently voting`);
+}
